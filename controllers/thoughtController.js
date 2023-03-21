7557c650c5d38e0bb32b5a9a390019002aa4dfa2
caernst13 <caleb_ernst@yahoo.com>
2023-03-20 22:27:44 -0400

    added update and delete thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,5 +27,26 @@ module.exports = {
         }).then((thought) => res.json(thought))
         .catch((err) => res.status(500).json(err));
     },
-    
+
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        ).then((thought) =>
+            !thought
+            ? res.status(404).json({ message: 'No thought with that ID' })
+            : res.json(thought)
+        ).catch((err) => res.status(500).json(err));
+    },
+
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+        .then((thought) =>
+            !thought
+            ? res.status(404).json({ message: 'No thought with that ID' })
+            : Student.deleteMany({ _id: { $in: thought.thoughts } })
+        ).then(() => res.json({ message: 'thought and thoughts!' }))
+        .catch((err) => res.status(500).json(err));
+    },
 }
